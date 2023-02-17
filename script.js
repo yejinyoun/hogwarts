0c793e902c44c78b0be2b660f313f5180cf4609a
yejinyoun <yejinyoun@icloud.com>
2023-02-17 19:34:50 +0100

    Json data loaded

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const allStudents = [];
+
+//nick name (if any), nickname (if any)
+const Student = {
+  firstName: "",
+  lastName: "",
+  middleName: undefined,
+  nickName: undefined,
+  gender: "",
+  photo: "",
+  house: "",
+};
+
+loadJSON();
+
+function loadJSON() {
+  fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      // when loaded, prepare objects
+      cleanData(jsonData);
+    });
+}
+
+function cleanData(jsonData) {
+  console.log(jsonData);
+}
+
+function prepareObjects() {}
